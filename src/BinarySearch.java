6cf16d4e8f646a6b3d2f7381a736a4a957d3fa38
Hassam <syedhassam2001@gmail.com>
2023-04-04 23:20:55 +0500

    added next smallest letter > target

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -69,4 +69,21 @@ public class BinarySearch {
         }
         return arr[end];
     }
+    public char letterNextToTarget(char [] arr, char target){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start <=end){
+            int mid=start + (end-start)/2;
+            if(target < arr[mid]){
+                end=mid -1;
+            }else if(target > arr[mid]){
+                start=mid+1;
+            }else{
+                return arr[mid];
+            }
+        }
+        return arr[start % arr.length];
+
+    }
 }
--- src/BinarySearchTest.java
+++ src/BinarySearchTest.java
@@ -11,6 +11,7 @@ class BinarySearchTest {
         BinarySearch bs=new BinarySearch();
         int [] arrRev={90,88,55,35,22,11};
         int [] arr={11,22,45,55,60,90};
+        char [] arrL={'b','c','f','e'};
         //testing that if BinarySearch Tests For Reverse
         assertThat(bs.orderAgnositc(arrRev,55), is(55));
         assertThat(bs.orderAgnositc(arr,45), is(45));
@@ -18,7 +19,8 @@ class BinarySearchTest {
         //so ceiling of a number means that number that is greater or equal to the target element
         assertThat(bs.ceilingNumber(arr,56), is(60));
         //so ceiling of a number means that number that is less then or equal to the target element
-        assertThat(bs.floorNumber(arr,56), is(55));
+        assertThat(bs.floorNumber(arr,49), is(49));
+        assertThat(bs.letterNextToTarget(arrL,'a'), is('b'));
 
 
     }
